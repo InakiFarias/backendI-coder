29b1dfcb571a579a43b2b47c11b64f8008b83afa
Inaki Farias <iniakifariasg@gmail.com>
2025-03-01 09:36:02 -0300

    Entrega final

--- src/routes/carts.route.js
+++ src/routes/carts.route.js
@@ -36,4 +36,61 @@ route.put("/:cid", async (req, res) => {
 
   res.status(201).json({ message: "Productos cargados", cart: cartUpdated })
 })
+
+route.post("/:cid/product/:pid", async (req, res) => {
+  const { cid, pid } = req.params
+
+  const cart = await cartModel.findById(cid)
+  if(!cart) return res.status(404).json({ message: "Carrito no encontrado"})
+
+  const indexProd = cart.products.findIndex(prod => prod.product.toString() === pid)
+  if(indexProd === -1) {
+    cart.products.push({ product: pid, quantity: 1})
+  } else {
+    cart.products[indexProd].quantity++
+  }
+
+  const cartUpdated = await cartModel.findByIdAndUpdate(cid, cart, { new: true }).populate("products.product")
+
+  res.status(201).json({ message: "Producto agregado", cart: cartUpdated })
+})
+
+route.put("/:cid/product/:pid", async (req, res) => {
+  const { cid, pid } = req.params
+  const { quantity } = req.body
+
+  if (!Number.isInteger(quantity) || quantity < 1) {
+    return res.status(400).json({ message: "Cantidad inválida" })
+  }
+
+  const cart = await cartModel.findById(cid)
+  if(!cart) return res.status(404).json({ message: "Carrito no encontrado"})
+
+  const indexProd = cart.products.findIndex(prod => prod.product.toString() === pid)
+  if(indexProd === -1) {
+      return res.status(404).json({ message: "Producto no encontrado" })
+  } else {
+    cart.products[indexProd].quantity = quantity
+  }
+
+  const cartUpdated = await cartModel.findByIdAndUpdate(cid, cart, { new: true }).populate("products.product")
+
+  res.status(201).json({ message: "Cantidad actualizada", cart: cartUpdated })
+})
+
+route.delete("/:cid/product/:pid", async (req, res) => {
+  const { cid, pid } = req.params
+
+  const cart = await cartModel.findById(cid)
+  if(!cart) return res.status(404).json({ message: "Carrito no encontrado"})
+
+  cart.products = cart.products.filter(prod => prod.product.toString() !== pid)
+
+  await cart.save()
+
+  const cartUpdated = await cartModel.findById(cid).populate("products.product")
+
+  res.status(201).json( {message: "Producto eliminado", cart: cartUpdated} )
+})
+
 export default route
